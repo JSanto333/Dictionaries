794eedb73ee8a6f773a60cb827453b7844604ecb
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-12 15:16:58 -0400

    Dictionaries final problems

--- Dictionaries/Program.cs
+++ Dictionaries/Program.cs
@@ -24,24 +24,75 @@ namespace Dictionaries
             //    Console.WriteLine(item);
             //}
 
-            Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
 
             //using add method to add drinks
 
-            drinkMachine.Add(10, "Ginger Ale");
-            drinkMachine.Add(11, "Bottled Water");
-            drinkMachine.Add(12, "Orange Juice");
-            drinkMachine.Add(13, "Fruit Punch");
-            drinkMachine.Add(14, "Mountain Dew Code Red");
+            //drinkMachine.Add(10, "Ginger Ale");
+            //drinkMachine.Add(11, "Bottled Water");
+            //drinkMachine.Add(12, "Orange Juice");
+            //drinkMachine.Add(13, "Fruit Punch");
+            //drinkMachine.Add(14, "Mountain Dew Code Red");
 
             //Counting the number of entries in the dictionary
-            // Console.WriteLine(drinkMachine.Count);
+            //Console.WriteLine(drinkMachine.Count);
 
-            foreach (KeyValuePair<int,string> drink in drinkMachine)
+            //foreach (KeyValuePair<int,string> drink in drinkMachine)
+            //{
+            //Console.WriteLine(drink.Key);
+            //Console.WriteLine(drink.Value);
+            //}
+
+            //Create a dictionary for a theater coat check with 10 elements
+            // the key will be a number and a value will be the coat style
+            //print all the elements to the console
+            //Dictionary<int, string> coatCheck = new Dictionary<int, string>();
+
+            //coatCheck.Add(1, " Leather");
+            //coatCheck.Add(2, " Mink");
+            //coatCheck.Add(3, " Rain jacket");
+            //coatCheck.Add(4, " Camo");
+            //coatCheck.Add(5, " Hoodie");
+            //coatCheck.Add(6, " Sport coat");
+            //coatCheck.Add(7, " Peacoat");
+            //coatCheck.Add(8, " Trench");
+            //coatCheck.Add(9, " Blazer");
+            //coatCheck.Add(10, " Parka");
+
+            //foreach (KeyValuePair<int,string> coat in coatCheck)
+            //{
+            //    Console.Write(coat.Key);
+            //    Console.WriteLine(coat.Value);
+            //}
+
+            //Car valet with 10 cars, the key will be a customers last name and the
+            //value will be the car make. print all the elements to the console
+            Dictionary<string, string> cars = new Dictionary<string, string>();
+
+            cars.Add("Jones", " Chevy");
+            cars.Add("Smith", " Ford");
+            cars.Add("Allen", " Dodge");
+            cars.Add("Hanks", " Honda");
+            cars.Add("Hunt", " Hyundai");
+            cars.Add("Kendrick", " Jeep");
+            cars.Add("Snow", " Volkswagen");
+            cars.Add("Sheen", " Lincoln");
+            cars.Add("Cruise", " Mercedes Benz");
+            cars.Add("Roberts", " Infiniti");
+
+            foreach (KeyValuePair<string,string> expensiveCars in cars)
             {
-                Console.WriteLine(drink.Key);
-                Console.WriteLine(drink.Value);
+                Console.Write(expensiveCars.Key);
+                Console.WriteLine(expensiveCars.Value);
             }
+
+            
+
+            //create a dictionary of 10 zoo animals, key will be the animal type
+            //and the value will be the number of animals
+            //Print the animal with the highest quantity to the console
+            //remove the zoo animal with the lowest count
+
         }
     }
 }
